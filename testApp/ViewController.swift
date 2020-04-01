62c363e6e11397fd6a2ba94ae0ab6a651c17eb27
koki <iosgitapp@gmail.com>
2020-04-02 02:25:45 +0900

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by 山口航輝 on 02/04/20.
+//  Copyright © 2020 koki.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
